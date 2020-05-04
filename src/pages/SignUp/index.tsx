41ac83e1c92ca9404b24a8f2596b0e387f4b4c8f
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2020-05-04 13:35:29 -0300

    Unform

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -8,13 +9,20 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignUp: React.FC = () => {
+  function handlesubmit(data: object): void {
+    console.log(data);
+  }
   return (
     <Container>
       <Background />
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <form>
+        <Form
+          onSubmit={() => {
+            handlesubmit({});
+          }}
+        >
           <h1>Faça seu cadastro</h1>
 
           <Input name="name" icon={FiUser} placeholder="Nome" />
@@ -28,7 +36,7 @@ const SignUp: React.FC = () => {
           />
 
           <Button type="submit">Cadastrar</Button>
-        </form>
+        </Form>
 
         <a href="login">
           <FiArrowLeft />
