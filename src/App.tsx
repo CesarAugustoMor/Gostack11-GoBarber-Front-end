a0feeff7cdfb9c7af90e637c93da27140aa7ba3b
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2020-05-05 17:17:32 -0300

    Estrutua e estilização de Toasts

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 
+import ToastContainer from './components/ToastContainer';
 import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => {
   return (
     <>
+      <ToastContainer />
       <AuthProvider>
         <SignIn />
       </AuthProvider>
