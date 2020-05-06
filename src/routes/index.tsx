e2df8d7aa7844e84d418de7657e081fa220ca8b5
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2020-05-06 14:04:44 -0300

    Rotas autenticadas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route component={SignIn} path="/" exact />
       <Route component={SignUp} path="/signup" />
+
+      <Route component={Dashboard} path="/dashboard" isPrivate />
     </Switch>
   );
 };
