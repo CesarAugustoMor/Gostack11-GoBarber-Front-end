3b8b98f08f24dcafa4917d4b58244717d6660b74
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2020-05-22 13:27:22 -0300

    Implementando redefinição

--- src/pages/ResetPassword/index.tsx
+++ src/pages/ResetPassword/index.tsx
@@ -2,12 +2,13 @@ import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import React, { useCallback, useRef } from 'react';
 import { FiLock } from 'react-icons/fi';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import * as Yup from 'yup';
 import logoImg from '../../assets/logo.svg';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 import { useToast } from '../../hooks/toast';
+import api from '../../services/api';
 import getValidationErrors from '../../utils/getValidationErrors';
 import { AnimatedContainer, Background, Container, Content } from './styles';
 
@@ -21,6 +22,7 @@ const ResetPassword: React.FC = () => {
 
   const { addToast } = useToast();
   const history = useHistory();
+  const location = useLocation();
 
   const handlesubmit = useCallback(
     async (data: ResetPasswordFormData) => {
@@ -39,10 +41,19 @@ const ResetPassword: React.FC = () => {
           abortEarly: false,
         });
 
-        // await ResetPassword({
-        //   email: data.email,
-        //   password: data.password,
-        // });
+        const token = location.search.replace('?token=', '');
+
+        if (!token) {
+          throw new Error();
+        }
+
+        const { password, password_confirmation } = data;
+
+        await api.post('password/reset', {
+          password,
+          password_confirmation,
+          token,
+        });
 
         history.push('/');
       } catch (error) {
@@ -52,6 +63,7 @@ const ResetPassword: React.FC = () => {
           formRef.current?.setErrors(errors);
           return;
         }
+
         addToast({
           type: 'error',
           title: 'Erro ao resetar senha',
@@ -59,7 +71,7 @@ const ResetPassword: React.FC = () => {
         });
       }
     },
-    [addToast, history],
+    [addToast, history, location.search],
   );
 
   return (
