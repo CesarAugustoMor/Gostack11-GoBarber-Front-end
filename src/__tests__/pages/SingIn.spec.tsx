915ee1bbe67da84cd14549e06b143e29774527b7
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2020-06-24 10:38:45 -0300

    Teste de login

--- src/__tests__/pages/SingIn.spec.tsx
+++ src/__tests__/pages/SingIn.spec.tsx
@@ -1,19 +1,33 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { useHistory } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
 import SignIn from '../../pages/SignIn';
+
+const mockedHistoryPush = jest.fn();
 
 jest.mock('react-router-dom', () => {
   return {
-    useHistory: jest.fn(),
+    useHistory: () => ({
+      push: mockedHistoryPush,
+    }),
     Link: ({ children }: { children: React.ReactNode }) => children,
   };
 });
 
 describe('SingIn Page', () => {
   it('should be able to sign in', () => {
-    const result = render(<SignIn />);
+    const { getByPlaceholderText, getByText } = render(<SignIn />);
 
-    result.debug();
+    const emailFiel = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('Senha');
+    const buttonElement = getByText('Entrar');
+
+    fireEvent.change(emailFiel, {
+      target: { valeu: 'johndoe@exemple.com' },
+    });
+    fireEvent.change(passwordField, { target: { valeu: '12345678' } });
+
+    fireEvent.click(buttonElement);
+
+    expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard');
   });
 });
